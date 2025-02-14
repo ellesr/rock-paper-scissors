286647bda5337a7e4cad8bf957a10b1489b3e8d1
ellesr <116410947+ellesr@users.noreply.github.com>
2025-02-14 12:52:59 -0800

    Write and test getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -3,11 +3,26 @@ create function getComputerChoice
 create three strings "rock" "paper" "scissors"
 assign value 0-2
 use math.floor on math.random * 2 to generate number between 0 and 2 
-randomly return one string
+randomly return one string:
+if computerChoice === rock, return "rock" .. else if .. else
 test with console.log
 */
 
-function getComputerChoice {
-    
+let computerChoice;
+
+function getComputerChoice(computerChoice) {
+    let rock = 0;
+    let paper = 1;
+    let scissors = 2;
+
+    computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice === 0) {
+        return computerChoice = "rock";
+    } else if (computerChoice === 1) {
+        return computerChoice = "paper";
+    } else (computerChoice === 2)
+        return computerChoice = "scissors";
 }
 
+console.log(getComputerChoice(computerChoice));
