09319be4a2d1341a8c7e9f20b6c58d4374aa1b28
ellesr <116410947+ellesr@users.noreply.github.com>
2025-02-14 12:33:20 -0800

    Write pseudocode

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/*
+create function getComputerChoice
+create three strings "rock" "paper" "scissors"
+assign value 0-2
+use math.floor on math.random * 2 to generate number between 0 and 2 
+randomly return one string
+test with console.log
+*/
+
+function getComputerChoice {
+    
+}
+
