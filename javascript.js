7378e0a6e4993e4fce493462570b14dbea75d0f0
ellesr <116410947+ellesr@users.noreply.github.com>
2025-03-04 16:00:56 -0800

    Add divs to display game progress and results

--- javascript.js
+++ javascript.js
@@ -25,22 +25,27 @@ btn.forEach(button => {
 });
 
 function playRound(humanChoice, computerChoice) {
+    choiceContent.textContent = `You chose ${humanChoice}. The computer chose ${computerChoice}.`;
     console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`);
 
     if (humanChoice == "rock" && computerChoice == "scissors" 
         || humanChoice == "scissors" && computerChoice == "paper"
         || humanChoice == "paper" && computerChoice == "rock") {
         ++humanScore;
+        winnerContent.textContent = 'You won this round!';
         console.log('You won this round!');
     } else if (computerChoice == "rock" && humanChoice == "scissors" 
         || computerChoice == "scissors" && humanChoice == "paper"
         || computerChoice == "paper" && humanChoice == "rock") {
         ++computerScore;
+        winnerContent.textContent = 'You lost this round!';
         console.log('You lost this round!');
     } else { (computerChoice == humanChoice) 
+        winnerContent.textContent = "It's a tie!";
         console.log("It's a tie!");
     }
 
+    scoreContent.textContent = `Your Score: ${humanScore}\nComputer's Score: ${computerScore}`;
     console.log(`Your Score: ${humanScore}\nComputer's Score: ${computerScore}\n\n`);
     playGame();
 }
@@ -48,10 +53,13 @@ function playRound(humanChoice, computerChoice) {
 function playGame() {
     if (humanScore + computerScore >= 5) {
         if (computerScore === humanScore) {
+            winnerContent.textContent = 'Good effort! You tied the game!';
             console.log('Good effort! You tied the game!');
         } else if (computerScore > humanScore) {
+            winnerContent.textContent = 'So sad! You lost the game!';
             console.log('So sad! You lost the game!');
         } else { (humanScore > computerScore) 
+            winnerContent.textContent = 'Congratulations! You won the game!';
             console.log('Congratulations! You won the game!');
         }
         resetGame();
@@ -63,3 +71,28 @@ function resetGame() {
     computerScore = 0;
     console.log(`\n* * * Begin New Game * * *\n\n`);
 }
+
+const title = document.querySelector("#title");
+const titleContent = document.createElement("div");
+titleContent.classList.add("titleContent");
+titleContent.textContent = 'Choose your player!';
+titleContent.style.cssText = "color: black; font-size: 50px;";
+title.appendChild(titleContent);
+
+
+const result = document.querySelector("#results");
+
+const choiceContent = document.createElement("div");
+choiceContent.classList.add("choiceContent");
+choiceContent.style.cssText = "color: black; font-size: 25px;";
+result.appendChild(choiceContent);
+
+const winnerContent = document.createElement("div");
+winnerContent.classList.add("winnerContent");
+winnerContent.style.cssText = "color: red; font-size: 25px;";
+result.appendChild(winnerContent);
+
+const scoreContent = document.createElement("div");
+scoreContent.classList.add("scoreContent");
+scoreContent.style.cssText = "color: black; font-size: 25px;";
+result.appendChild(scoreContent);
